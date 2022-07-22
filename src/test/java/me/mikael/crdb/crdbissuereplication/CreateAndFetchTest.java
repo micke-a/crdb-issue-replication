47dae5aad97acb7bd739fcbeeda7287426cb7092
Mikael <mail.micke@gmail.com>
2022-07-22 09:46:07 +0100

    .

--- src/test/java/me/mikael/crdb/crdbissuereplication/CreateAndFetchTest.java
+++ src/test/java/me/mikael/crdb/crdbissuereplication/CreateAndFetchTest.java
@@ -14,7 +14,9 @@ public class CreateAndFetchTest {
         var restTemplate = new RestTemplateBuilder().build();
         var createUrl = "http://localhost:8080/issue/create";
         var fetchUrl = "http://localhost:8080/issue/fetch/";
-        for(int i=0; i< 10; i++){
+        int successCount = 0;
+        int errorCount = 0;
+        for(int i=0; i< 100; i++){
             var createResponse = restTemplate.postForEntity(createUrl, null,  String.class);
             var createdId = createResponse.getBody();
 
@@ -22,10 +24,13 @@ public class CreateAndFetchTest {
 
             if(fetchResponse.getBody().contains("Not Found")){
                 System.out.printf("ERROR - Created entity with id %s , Not Found, message: %s %n", createdId, fetchResponse.getBody());
+                errorCount++;
             }
             else{
                 System.out.printf("SUCCESS - Entity with id %s found%n", createdId);
+                successCount++;
             }
         }
+        System.out.printf("Results success: %s, error: %s", successCount, errorCount);
     }
 }
